67cfd086b0ea639fc30b902bd359192b8763afc7
giggsgog <32261396+giggsgog@users.noreply.github.com>
2017-10-10 11:49:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -23,11 +23,7 @@ if (!is_null($events['events'])) {
 					{
 						'type' => 'text',
 						'text' => $text
-					},
-					{
-						'type' => 'text',
-						'text' => "ว่าไง"
-					}
+					}					
 				];
 			}
 			
